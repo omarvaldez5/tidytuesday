4b60ae18837c598599b3854e18a143a26dbfb7e6
omarvaldez5 <ovaldez@tuta.io>
2022-05-23 16:04:33 -0600

    created map for tuskegee

--- 2022/W06_tuskegee/code.py
+++ 2022/W06_tuskegee/code.py
@@ -10,6 +10,7 @@ import pandas as pd
 import janitor as jr
 import valdezds as vds
 import geopandas as gp
+import plotnine as p9
 
 vds.getwd() # Confirm working directory
 
@@ -32,6 +33,7 @@ df.isnull().sum()
 df.shape
 vds.select_by_number(df, 3, 7, 5, 14).describe()
 
+
 # ============================================================================ #
 # 2.0 Data Wrangling
 # ============================================================================ #
@@ -49,28 +51,39 @@ vds.select_by_number(df, 3, 7, 5, 14).describe()
 # 2.1 Python
 # ============================================================================ #
 
+# https://towardsdatascience.com/interactive-geographical-maps-with-geopandas-4586a9d7cc10
+# https://geopandas.org/en/stable/docs/user_guide/mapping.html
+# https://coderzcolumn.com/tutorials/data-science/maps-using-plotnine-choropleth-scatter-bubble-maps
 
-path_to_data = gp.datasets.get_path("nybb")
-gdf = gp.read_file(path_to_data)
+# Example World data
+world_filepath = gp.datasets.get_path("naturalearth_lowres")
+world = gp.read_file(world_filepath)
+world.to_clipboard()
+united_states = world.query("iso_a3 == 'USA'")  # USA filter
 
-gdf = gdf.set_index("BoroName")
-gdf["area"] = gdf.area
-gdf["area"]
+# Getting united states geographical data from another source
+us_states_geo = gp.read_file("https://raw.githubusercontent.com/sunny2309/datasets/master/us-states.json")
+us_states_geo.to_clipboard()
 
+# Join
+us_map = pd.merge(us_states_geo, df, how="left", left_on="id", right_on="state")
 
-gdf['boundary'] = gdf.boundary
-gdf['boundary']
+# Total count
+size_count = (us_map.pipe(vds.select_by_number, 10, 12)
+              .groupby(["state"])
+              .size()
+              .to_frame("n_size")
+              .reset_index()
+              .sort_values("n_size", ascending=False)
+              )
 
-
-gdf['centroid'] = gdf.centroid
-gdf['centroid']
-
-
-gdf.plot("area", legend=True)
+# Merge with main data
+us_count = pd.merge(us_map, size_count, how="left", on="state")
 
 
 # ============================================================================ #
 # 2.1.1 Save to CSV
+us_count.to_csv("2022/W06_tuskegee/data_tuskgee.csv", index=False)
 
 
 # ============================================================================ #
@@ -82,11 +95,14 @@ from pandasql import sqldf
 pysqldf = lambda q: sqldf(q, globals()) # Quicker to write query
 
 q = """
+    SELECT * FROM us_map
     ;
     """
 
 pysqldf(q)
 
+# Error: https://docs.sqlalchemy.org/en/14/errors.html#error-rvf5
+# Search
 
 # ============================================================================ #
 # 3.0 Plot
@@ -95,6 +111,41 @@ pysqldf(q)
 # ============================================================================ #
 # 3.1 Plotnine
 # ============================================================================ #
+
+# Geom Map
+
+(
+    p9.ggplot() +
+    
+    # geoms
+    p9.geom_map(data=us_count, mapping=p9.aes(fill="n_size", color="n_size")) +
+    p9.scale_fill_cmap(cmap_name="RdYlBu") +
+    p9.scale_color_cmap(cmap_name="RdYlBu") +
+    p9.xlim(-170, -60) +
+    p9.ylim(25, 72) +
+    
+    # labs
+    p9.labs(x="",
+            y="",
+            title="Tuskegee Airmen - Qty Pilot Types from U.S.A") +
+    
+    # theme
+    p9.theme(
+        subplots_adjust={'hspace': 0.25},
+        figure_size=(10,6),
+        panel_background=p9.element_rect(fill="snow"),
+        legend_title=p9.element_text(face="bold"),
+        legend_text=p9.element_text(face="italic"),
+        plot_title=p9.element_text(face="bold", size=15),
+        axis_text_x=p9.element_blank(),
+        axis_text_y=p9.element_blank(),
+        axis_ticks_major_x=p9.element_blank(),
+        axis_ticks_major_y=p9.element_blank()
+    ) +
+    
+    # watermark
+    p9.watermark("./iconvaldezdata.png")
+)
 
 # ============================================================================ #
 # 3.2 Tableau
